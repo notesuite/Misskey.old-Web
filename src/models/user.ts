29a221939ffb234680536ab7fb6eb36df7d28980
syuilo <syuilotan@yahoo.co.jp>
2015-10-15 07:59:31 +0900

    Implement User interface

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+export interface User {
+	id: string;
+	createdAt: Date;
+	name: string;
+	screenName: string;
+	comment: string;
+	description: string;
+	color: string;
+	email: string;
+	lang: string;
+	location: string;
+	credit: number;
+	pinnedStatus: string;
+	websiteUrl: string;
+	iconUrl: string;
+	bannerUrl: string;
+	wallpaperUrl: string;
+	birthday: Date;
+	isVerfied: boolean;
+	isEmailVerified: boolean;
+	isPro: boolean;
+	isPrivate: boolean;
+	isSuspended: boolean;
+	isDeleted: boolean;
+}
