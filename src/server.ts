075b8b0961b1c978bd77e3ca7c53e005123d0838
syuilo <syuilotan@yahoo.co.jp>
2015-10-19 18:48:35 +0900

    Commentout unused server

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,9 @@ const server: http.Server = http.createServer(app);
 
 // Define servers
 const mainServer: express.Express = require(`${__dirname}/web/main`).server;
-const devServer: express.Express = require(`${__dirname}/web/dev`).server;
+// const devServer: express.Express = require(`${__dirname}/web/dev`).server;
 app.use(vhost('misskey.xyz', mainServer));
-app.use(vhost('dev.misskey.xyz', devServer));
+// app.use(vhost('dev.misskey.xyz', devServer));
 
 // Listen core app
 server.listen(config.port.webHttp, () => {
