81a37eeed3935c63fb74fbd60d938ba8b0edf304
syuilo <syuilotan@yahoo.co.jp>
2015-10-19 18:52:28 +0900

    Fix bug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@
 import * as http from 'http';
 import * as express from 'express';
 // import * as vhost from 'vhost';
+import config from 'config';
 
-const config: any = require('./config');
 const vhost: any = require('vhost');
 
 console.log('Servers loader loaded');
@@ -21,7 +21,7 @@ app.use(vhost('misskey.xyz', mainServer));
 // app.use(vhost('dev.misskey.xyz', devServer));
 
 // Listen core app
-server.listen(config.port.webHttp, () => {
+server.listen(config.port.http, () => {
 	const host: string = server.address().address;
 	const port: number = server.address().port;
 
