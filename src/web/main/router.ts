ba04d7bf40c8bd20886d88f22daa6064afdbcda6
syuilo <syuilotan@yahoo.co.jp>
2015-10-19 19:49:29 +0900

    [WIP] Implement router

--- src/web/main/router.ts
+++ src/web/main/router.ts
@@ -1,6 +1,31 @@
 import * as express from 'express';
 
+import { MisskeyExpressRequest } from '../misskeyExpressRequest';
+import { MisskeyExpressResponse } from '../misskeyExpressResponse';
+
+function callController(req: MisskeyExpressRequest, res: MisskeyExpressResponse, name: string, options?: any) {
+	switch (req.ua) {
+		case 'desktop':
+			require("./sites/desktop/controllers/" + name)(req, res, options);
+			break;
+		case 'mobile':
+			require("./sites/mobile/controllers/" + name)(req, res, options);
+			break;
+		default:
+			require("./sites/desktop/controllers/" + name)(req, res, options);
+			break;
+	}
+}
+
 export default function(app: express.Express): void {
 	'use strict';
 	console.log('Init Web router');
+	
+	app.get('/', (req: MisskeyExpressRequest, res: MisskeyExpressResponse) => {
+		if (req.isLogin) {
+			callController(req, res, 'home');
+		} else {
+			callController(req, res, 'entrance');
+		}
+	});
 }
