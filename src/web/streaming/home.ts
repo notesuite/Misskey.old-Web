e2b32920e8af7a3de559fd045cd531e4ab62fb70
syuilo <syuilotan@yahoo.co.jp>
2015-11-02 05:57:55 +0900

    wip

---
+++ src/web/streaming/home.ts
@@ -0,0 +1,40 @@
+import * as redis from 'redis';
+import * as SocketIO from 'socket.io';
+import * as cookie from 'cookie';
+import config from '../../config';
+
+interface MKSocketIOSocket extends SocketIO.Socket {
+	user: any
+}
+
+module.exports = (io: SocketIO.Server, sessionStore: any) => {
+	io.of('/streaming/home').on('connection', (socket: MKSocketIOSocket) => {
+		// Connect to Redis
+		const subscriber: redis.RedisClient = redis.createClient();
+
+		// Get cookies
+		const cookies: { [key: string]: string } = cookie.parse(socket.handshake.headers.cookie);
+
+		// Get sesson key
+		const sid: string = cookies[config.sessionKey];
+		const sidkey: string = sid.match(/s:(.+?)\./)[1];
+
+		// Resolve session
+		sessionStore.get(sidkey, (err: any, session: any) => {
+			if (err !== null) {
+				return console.error(err);
+			} else if (session === null) {
+				return;
+			}
+
+			// Get user
+			socket.user = session.user;
+
+			// Subscribe Home stream channel
+			subscriber.subscribe(`misskey:userStream:${socket.user.id}`);
+			subscriber.on('message', (_: any, content: string) =>{
+				content = JSON.parse(content);
+			});
+		});
+	});
+};
--- src/web/streaming/index.ts
+++ src/web/streaming/index.ts
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import * as session from 'express-session';
 import * as SocketIO from 'socket.io';
+import * as cookie from 'cookie';
 import config from '../../config';
 
 console.log('Init Web streaming server');
@@ -20,18 +21,19 @@ const sessionStore: any = new RedisStore({
 	prefix: 'misskey-session:'
 });
 
-io.use((socket: SocketIO.Socket, next: (err: any) => void) => {
-/*  rawCookie = socket != null ? (ref$ = socket.request) != null ? (ref1$ = ref$.headers) != null ? ref1$.cookie : void 8 : void 8 : void 8;
-  if (cookie) {
-    parsedCookie = cookie.parse(rawCookie);
-    isAuthorized = !/s:(.+?)\./.test(parsedCookie[config.sessionKey]);
-    if (isAuthorized) {
-      return next(new Error('[[error:not-authorized]]'));
-    } else {
-      return next();
-    }
-  }
-*/
+// Authorization
+io.use((socket: SocketIO.Socket, next: (err?: any) => void) => {
+	const rawCookie: string = socket.request.headers.cookie;
+	const parsedCookie: { [key: string]: string } = cookie.parse(rawCookie);
+	const isAuthorized: boolean = /s:(.+?)\./.test(parsedCookie[config.sessionKey]);
+	if (isAuthorized) {
+		next();
+	} else {
+		next(new Error('[[error:not-authorized]]'));
+	}
 });
+
+// Home stream
+require('./home')(io, sessionStore);
 
 server.listen(config.port.streaming);
