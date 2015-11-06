0c6557bbfd1ca4aca3a0f0ae254b4f9376be91ca
syuilo <syuilotan@yahoo.co.jp>
2015-11-06 22:03:48 +0900

    ud

--- src/web/resources/desktop/scripts/lib/waves-effect.js
+++ src/web/resources/desktop/scripts/lib/waves-effect.js
@@ -28,6 +28,20 @@
 	'use strict';
 
 	var WavesEffect = function WavesEffect(){};
+	
+	function distance(p, q) {
+		var sqrt = Math.sqrt, pow = Math.pow;
+		return sqrt(pow(p.x - q.x, 2) + pow(p.y - q.y, 2));
+	}
+
+	function calcCircleScale(boxW, boxH, circleCenterX, circleCenterY) {
+		var origin = {x: circleCenterX, y: circleCenterY};
+		var dist1 = distance({x: 0, y: 0}, origin);
+		var dist2 = distance({x: boxW, y: 0}, origin);
+		var dist3 = distance({x: 0, y: boxH}, origin);
+		var dist4 = distance({x: boxW, y: boxH }, origin);
+		return Math.max(dist1, dist2, dist3, dist4) * 2;
+	}
 
 	WavesEffect.attachToClass = function(className) {
 		var attachees = document.getElementsByClassName(className);
@@ -56,28 +70,27 @@
 				ripple.style.backgroundColor = 'rgba(0, 0, 0, 0.15)';
 				ripple.style.opacity = '1';
 				ripple.style.transform = 'scale(1, 1)';
-				ripple.style.transition = 'all 1.5s cubic-bezier(0, .5, .5, 1)';
+				ripple.style.transition = 'all 1s cubic-bezier(0,.75,.25,1)';
 				rippleContainer.appendChild(ripple);
 				event.target.appendChild(rippleContainer);
 				
 				var boxW = event.target.clientWidth;
 				var boxH = event.target.clientHeight;
-				var circleX = event.pageX - event.target.offsetLeft;
-				var circleY = event.pageY - event.target.offsetTop;
+				var circleCenterX = event.pageX - event.target.offsetLeft;
+				var circleCenterY = event.pageY - event.target.offsetTop;
 				
-				var zure = Math.pow((circleX - (boxW / 2)) * (circleX - (boxW / 2)) + (circleY - (boxH / 2)) * (circleY - (boxH / 2)), 0.5);
-				var scale = (Math.max(boxW, boxH) * Math.sqrt(2)) + (zure * Math.sqrt(2) * Math.sqrt(2));
+				var scale = calcCircleScale(boxW, boxH, circleCenterX, circleCenterY);
 				
 				setTimeout(function() {
 					ripple.style.transform = 'scale(' + (scale / 2) + ', ' + (scale / 2) + ')';
 				}, 1);
 				setTimeout(function() {
-					ripple.style.transition = 'all 1.5s ease';
+					ripple.style.transition = 'all 1s ease';
 					ripple.style.opacity = '0';
-				}, 1500);
+				}, 1000);
 				setTimeout(function() {
 					event.target.removeChild(rippleContainer); 
-				}, 3000);
+				}, 2000);
 			}, false);
 		});
 	};
