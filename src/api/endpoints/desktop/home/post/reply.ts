dd2caec3ec90619343bdc047fe2fba399b10ec61
syuilo <syuilotan@yahoo.co.jp>
2015-11-11 11:03:40 +0900

    fix bog

--- src/api/endpoints/desktop/home/post/reply.ts
+++ src/api/endpoints/desktop/home/post/reply.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import * as express from 'express';
-import { MisskeyExpressRequest } from '../../../../misskeyExpressRequest';
-import { MisskeyExpressResponse } from '../../../../misskeyExpressResponse';
-import requestApi from '../../../../utils/requestApi';
+import { MisskeyExpressRequest } from '../../../../../misskeyExpressRequest';
+import { MisskeyExpressResponse } from '../../../../../misskeyExpressResponse';
+import requestApi from '../../../../../utils/requestApi';
 
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
