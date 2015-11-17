26ef0bea8c7d46890e9e6d38798c0314d2af167f
syuilo <syuilotan@yahoo.co.jp>
2015-11-18 03:05:14 +0900

    commit

--- src/api/endpoints/desktop/home/posts/reply.ts
+++ src/api/endpoints/desktop/home/posts/reply.ts
@@ -4,7 +4,14 @@ import requestApi from '../../../../../utils/requestApi';
 
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
-	requestApi('POST', 'posts/reply', req.body, req.session.userId).then((reply: Object) => {
-		res.send('kyoppie');
-	});
+	const photos: string = req.body['photos'];
+	if (photos !== undefined && photos !== null) {
+		requestApi('POST', 'posts/photo', req.body, req.session.userId).then((reply: Object) => {
+			res.send('kyoppie');
+		});
+	} else {
+		requestApi('POST', 'posts/status', req.body, req.session.userId).then((reply: Object) => {
+			res.send('kyoppie');
+		});
+	}
 };
