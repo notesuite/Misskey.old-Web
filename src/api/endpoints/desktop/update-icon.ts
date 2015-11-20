55dfe62638b62ec6a640cdde2075c49dfed55824
syuilo <syuilotan@yahoo.co.jp>
2015-11-21 03:35:09 +0900

    commit

--- src/api/endpoints/desktop/update-icon.ts
+++ src/api/endpoints/desktop/update-icon.ts
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import * as request from 'request';
-import * as gm from 'gm';
+// import * as gm from 'gm';
+const gm: any = require('gm').subClass({imageMagick: true});
 import { MisskeyExpressRequest } from '../../../misskeyExpressRequest';
 import { MisskeyExpressResponse } from '../../../misskeyExpressResponse';
 import requestApi from '../../../utils/requestApi';
@@ -8,38 +9,45 @@ import requestApi from '../../../utils/requestApi';
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
 	const avaterFileId: string = req.body['file-id'];
-	const trimX: number = req.body['trim-x'];
-	const trimY: number = req.body['trim-y'];
-	const trimW: number = req.body['trim-w'];
-	const trimH: number = req.body['trim-h'];
+	const trimX: number = Number(req.body['trim-x']);
+	const trimY: number = Number(req.body['trim-y']);
+	const trimW: number = Number(req.body['trim-w']);
+	const trimH: number = Number(req.body['trim-h']);
 
 	requestApi('GET', 'album/files/show', {
 		'file-id': avaterFileId
 	}, req.session.userId).then((file: any) => {
 		if (file.dataSize > 3000000) {
-			return res.send(500, 'big-data');
+			return res.status(500).send('big-data');
 		}
-		request(file.url, (getFileErr: any, response: http.IncomingMessage, body: string) => {
-			gm(body)
+		request({
+			url: file.url,
+			encoding: null
+		}, (getFileErr: any, response: http.IncomingMessage, body: Buffer) => {
+			if (getFileErr !== null) {
+				console.error(getFileErr);
+				return res.status(500).send(getFileErr);
+			}
+			gm(body, file.name)
 				.crop(trimW, trimH, trimX, trimY)
-				.toBuffer('png', (err: Error, buffer: Buffer) =>
-			{
-				if (err !== null) {
-					console.error(err);
-					return res.send(500, err);
-				}
-				requestApi('POST', 'album/files/upload', {
-					file: {
-						value: buffer,
-						options: {
-							filename: `cropped-${file.name}`,
-							contentType: 'image/png'
-						}
+				.toBuffer('png', (err: Error, buffer: Buffer) => {
+					console.log(buffer);
+					if (err !== null) {
+						console.error(err);
+						return res.status(500).send(err);
 					}
-				}, req.session.userId).then((albumFile: Object) => {
-					res.send(albumFile);
+					requestApi('POST', 'album/files/upload', {
+						file: {
+							value: buffer,
+							options: {
+								filename: `cropped-${file.name}`,
+								contentType: 'image/png'
+							}
+						}
+					}, req.session.userId).then((albumFile: Object) => {
+						res.send(albumFile);
+					});
 				});
-			});
 		});
 	});
 };
