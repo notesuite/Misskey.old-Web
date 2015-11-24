94c83741f6142540f293d5f6da9584464351a82f
syuilo <syuilotan@yahoo.co.jp>
2015-11-25 08:48:44 +0900

    Fix bug

--- src/api/endpoints/sites/desktop/home/posts/timeline.ts
+++ src/api/endpoints/sites/desktop/home/posts/timeline.ts
@@ -1,20 +1,19 @@
+import * as express from 'express';
 const jade: any = require('jade');
-import { MisskeyExpressRequest } from '../../../../../../misskeyExpressRequest';
-import { MisskeyExpressResponse } from '../../../../../../misskeyExpressResponse';
 import requestApi from '../../../../../../utils/requestApi';
 import parsePostText from '../../../../../../utils/parsePostText';
 import config from '../../../../../../config';
 
-export default function timeline(req: MisskeyExpressRequest, res: MisskeyExpressResponse): void {
+export default function timeline(req: express.Request, res: express.Response): void {
 	'use strict';
 
 	const compiler: (locals?: any) => string = jade.compileFile(
-		`${__dirname}/../../../../../sites/desktop/views/lib/post/smart/posts.jade`);
+		`${__dirname}/../../../../../../sites/desktop/views/lib/post/smart/posts.jade`);
 
-	requestApi('GET', 'posts/timeline', req.query, req.session.userId).then((tl: Object[]) => {
+	requestApi('GET', 'posts/timeline', req.query, (<any>req.session).userId).then((tl: Object[]) => {
 		res.send(compiler({
 			posts: tl,
-			me: req.me,
+			me: req.user,
 			parsePostText: parsePostText,
 			config: config.publicConfig
 		}));
