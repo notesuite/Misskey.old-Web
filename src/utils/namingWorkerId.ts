24a971e4894c5e0ae926ab87a147ad2c952ab05b
syuilo <syuilotan@yahoo.co.jp>
2015-11-28 05:37:26 +0900

    ud

--- src/utils/namingWorkerId.ts
+++ src/utils/namingWorkerId.ts
@@ -18,6 +18,30 @@ export default function namingWorkerId(workerId: string): string {
 			return '綾乃';
 		case '8':
 			return '千歳';
+		case '9':
+			return '千鶴';
+		case '10':
+			return 'りせ';
+		case '11':
+			return '西垣';
+		case '12':
+			return '花子';
+		case '13':
+			return '撫子';
+		case '14':
+			return 'あかね';
+		case '15':
+			return 'ともこ';
+		case '16':
+			return 'めぐみ';
+		case '17':
+			return '藍';
+		case '18':
+			return '美穂';
+		case '19':
+			return 'こころ';
+		case '20':
+			return 'みさき';
 		default:
 			return workerId;
 	}
