5250ecaf9c1c1ce2cfac78ce4d87930c125e0af0
syuilo <syuilotan@yahoo.co.jp>
2015-12-04 13:24:39 +0900

    c

---
+++ src/sites/mobile/pages/i/mentions/controller.ts
@@ -0,0 +1,13 @@
+import { MisskeyExpressRequest } from '../../../../../misskey-express-request';
+import { MisskeyExpressResponse } from '../../../../../misskey-express-response';
+import requestApi from '../../../../../utils/request-api';
+
+module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
+	'use strict';
+
+	requestApi('posts/mentions', { 'limit': 10 }, req.me).then((mentions: any[]) => {
+		res.display({
+			mentions: mentions
+		});
+	});
+};
