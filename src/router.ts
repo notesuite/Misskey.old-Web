22585f730603e512d188d46dea7aa55c4cebe686
marihachi <marihachi0620@gmail.com>
2015-12-06 20:30:45 +0900

    remove GET /logout

--- src/router.ts
+++ src/router.ts
@@ -87,14 +87,6 @@ export default function router(app: express.Express): void {
 		}
 	}
 
-	app.get('/logout', (req: MisskeyExpressRequest, res: MisskeyExpressResponse) => {
-		if (req.isLogin) {
-			req.session.destroy(function() {
-				res.redirect('/');
-			});
-		}
-	}
-
 	app.get('/welcome', (req: MisskeyExpressRequest, res: MisskeyExpressResponse) => {
 		callController(req, res, 'welcome');
 	});
