0b725a5b7f084ffbb024b21ddb077432af2d5bde
syuilo <syuilotan@yahoo.co.jp>
2015-12-09 01:09:00 +0900

    c

--- src/sites/desktop/pages/entrance/controller.ts
+++ src/sites/desktop/pages/entrance/controller.ts
@@ -1,7 +1,12 @@
 import { MisskeyExpressRequest } from '../../../../misskey-express-request';
 import { MisskeyExpressResponse } from '../../../../misskey-express-response';
+import requestApi from '../../../../utils/request-api';
 
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
-	res.display();
+	requestApi('hashtags/trend/show', {}).then((hashtags: string[]) => {
+		res.display({
+			trends: hashtags
+		});
+	});
 };
