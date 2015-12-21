4b410175fbfccf4a800060ff1199807f1e72be87
syuilo <syuilotan@yahoo.co.jp>
2015-12-22 04:25:32 +0900

    c

--- src/api/streaming/talk.ts
+++ src/api/streaming/talk.ts
@@ -6,27 +6,37 @@ import config from '../../config';
 
 interface MKSocketIOSocket extends SocketIO.Socket {
 	user: any;
+}
+
+interface MKUserSocketIOSocket extends MKSocketIOSocket {
 	otherpartyId: string;
 }
 
-module.exports = (io: SocketIO.Server, sessionStore: any) => {
-	io.of('/streaming/talk').on('connection', (socket: MKSocketIOSocket) => {
-		getSession(socket, sessionStore).then((session: any) => {
-			// Get user
-			socket.user = session.user;
+interface MKGroupSocketIOSocket extends MKSocketIOSocket {
+	groupId: string;
+}
 
-			// Connect to Redis
-			const subscriber: redis.RedisClient = redis.createClient(
-				6379, config.redisServerHost, <redis.ClientOpts>{
-				disable_resubscribing: true
-			});
+function createRedisClient(): redis.RedisClient {
+	'use strict';
+
+	return redis.createClient(
+		6379, config.redisServerHost, <redis.ClientOpts>{
+		disable_resubscribing: true
+	});
+}
+
+module.exports = (io: SocketIO.Server, sessionStore: any) => {
+	io.of('/streaming/talk').on('connection', (socket: MKUserSocketIOSocket) => {
+		getSession(socket, sessionStore).then((session: any) => {
+			socket.user = session.user;
+			const subscriber = createRedisClient();
 
 			socket.emit('connected');
+
 			socket.on('init', (req: any) => {
 				const otherpartyId: string = req['otherparty-id'];
 				socket.otherpartyId = otherpartyId;
 
-				// Subscribe Talk stream channel
 				subscriber.subscribe(`misskey:talk-user-stream:${socket.user.id}-${socket.otherpartyId}`);
 				subscriber.on('message', onStreamMessage);
 			});
@@ -39,7 +49,48 @@ module.exports = (io: SocketIO.Server, sessionStore: any) => {
 		function onStreamMessage(_: any, contentString: string): void {
 			'use strict';
 
-			// メッセージはJSONなのでパース
+			const content: any = JSON.parse(contentString);
+
+			switch (content.type) {
+				case 'message':
+					const messageId: any = content.value.id;
+
+					requestApi('talks/messages/show', {
+						'message-id': messageId
+					}, socket.user.id).then((message: Object) => {
+						socket.emit(content.type, message);
+					});
+					break;
+				default:
+					socket.emit(content.type, content.value);
+					break;
+			}
+		}
+	});
+
+	io.of('/streaming/group-talk').on('connection', (socket: MKGroupSocketIOSocket) => {
+		getSession(socket, sessionStore).then((session: any) => {
+			socket.user = session.user;
+			const subscriber = createRedisClient();
+
+			socket.emit('connected');
+
+			socket.on('init', (req: any) => {
+				const groupId: string = req['group-id'];
+				socket.groupId = groupId;
+
+				subscriber.subscribe(`misskey:talk-group-stream:${socket.groupId}`);
+				subscriber.on('message', onStreamMessage);
+			});
+
+			socket.on('disconnect', () => {
+				subscriber.end();
+			});
+		});
+
+		function onStreamMessage(_: any, contentString: string): void {
+			'use strict';
+
 			const content: any = JSON.parse(contentString);
 
 			switch (content.type) {
