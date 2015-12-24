ad74ae2365200b7009e9b8be426326e95c18f6db
syuilo <syuilotan@yahoo.co.jp>
2015-12-24 21:40:00 +0900

    c

--- src/sites/desktop/pages/i/talks/groups/controller.ts
+++ src/sites/desktop/pages/i/talks/groups/controller.ts
@@ -8,9 +8,7 @@ module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void
 	requestApi('talks/history/show', {
 		type: 'group'
 	}, req.me.id).then((messages: any[]) => {
-		console.log(messages);
 		requestApi('talks/group/invitations/show', {}, req.me.id).then((invitations: any[]) => {
-			console.log(invitations);
 			res.display({
 				messages: messages,
 				invitations: invitations
