fd7e73aaf1018ea4e4f559052ee7e8000f55b274
syuilo <syuilotan@yahoo.co.jp>
2015-12-26 12:15:40 +0900

    c

--- src/sites/desktop/common/scripts/lib/snow-storm.js
+++ src/sites/desktop/common/scripts/lib/snow-storm.js
@@ -6,15 +6,20 @@ var $ = require('jquery');
 
 $(function() {
 	$('html').css('overflow-x', 'hidden');
+	
+	var crystals = [];
 
 	function setSnow(top, left) {
-		if ($('body').children('.snow').length < 50) {
+		if (crystals.length < 300) {
 			var r = Math.random();
-			var size = Math.floor(2 + (r * 4));
+			var sizeMin = 2;
+			var sizeMax = 6;
+			var size = Math.floor(sizeMin + (r * sizeMax));
+			var speed = r;
+
 			var $snow = $('<div/>');
 			$snow.attr({
 				'class': 'snow',
-				'data-speed': r,
 			});
 			$snow.css({
 				'pointer-events' : 'none',
@@ -28,8 +33,16 @@ $(function() {
 				'background' : '#fff',
 				'border-radius' : '100%',
 				'box-shadow' : '0 0 8px #0ff',
+				'opacity' : '1',
+				'bottom' : '0px'
 			});
 			$('body').append($snow);
+			
+			crystals.push({
+				speed: speed,
+				size: size,
+				$snow: $snow
+			});
 		}
 	}
 
@@ -56,30 +69,53 @@ $(function() {
 	});
 
 	setInterval(function() {
-		$('body').children('.snow').each(function() {
+		crystals.forEach(function(crystal, index, object) {
 			var windowWidth = $(window).width(), windowHeight = $(window).height();
 
-			var $snow = $(this);
+			var $snow = crystal.$snow;
 			var snowOffset = $snow.offset();
-			var speed = parseFloat($snow.attr('data-speed'));
-			var wind = ((windowWidth / 2) - (parseFloat($('html').attr('cursorX')))) / (300 - (speed * 260));
-			var swing = Math.sin(snowOffset.top / (100 * speed)) * speed;
+			var wind = ((windowWidth / 2) - (parseFloat($('html').attr('cursorX')))) / (300 - (crystal.speed * 260));
+			var swing = Math.sin(snowOffset.top / (100 * crystal.speed)) * (crystal.speed * 3);
 
-			var top = snowOffset.top + (1 + (speed * 6));
+			var top = snowOffset.top + (1 + (crystal.speed * 6));
 			var left = snowOffset.left + wind + swing;
 
 			if (left > windowWidth || left < 0) { // 左右にはみ出した時
-				$snow.remove();
+				removeCrystal(false);
 			} else if (top < $(window).scrollTop()) { // 上にはみ出した時
-				$snow.remove();
+				removeCrystal(false);
 			} else if ((top + $snow.height()) > $(window).scrollTop() + windowHeight) { // 地面についた時
-				$snow.remove();
+				removeCrystal(true);
 			} else {
 				$snow.css({
 					'top' : top + 'px',
 					'left' : left + 'px',
 				});
 			}
+			
+			function removeCrystal(pile) {
+				object.splice(index, 1);
+				console.log((crystal.size / 2) + 'px');
+				
+				if (pile) {
+					$snow.css('transition', 'opacity 10s linear, width 10s linear, bottom 10s linear, left 10s linear');
+					
+					$snow.css({
+						'position': 'fixed',
+						'top': '',
+						'bottom': (-crystal.size) + 'px',
+						'left': (left - (crystal.size / 2)) + 'px',
+						'width': (crystal.size * 4) + 'px',
+						'opacity': '0'
+					});
+					
+					setTimeout(function() {
+						$snow.remove();
+					}, 10000);
+				} else {
+					$snow.remove();
+				}
+			}
 		});
-	}, 50);
+	}, 30);
 });
