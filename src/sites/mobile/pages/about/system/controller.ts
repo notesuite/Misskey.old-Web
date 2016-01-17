58dd2236b5e67adcdce806eb535e085fdd8426e4
syuilo <syuilotan@yahoo.co.jp>
2016-01-17 13:49:16 +0900

    c

--- src/sites/mobile/pages/about/system/controller.ts
+++ src/sites/mobile/pages/about/system/controller.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
+import config from '../../../../../config';
 
 module.exports = (req: express.Request, res: express.Response): void => {
 	'use strict';
 	res.locals.display({
-		env: process.env.NODE_ENV
+		env: process.env.NODE_ENV,
+		tls: config.https.enable,
+		domain: config.publicConfig.domain
 	});
 };
