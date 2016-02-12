c5c8747200d1326f3d1e5196afa546ed073fa00a
syuilo <syuilotan@yahoo.co.jp>
2016-02-12 16:32:15 +0900

    WIP

--- src/sites/mobile/pages/i/album/file/controller.ts
+++ src/sites/mobile/pages/i/album/file/controller.ts
@@ -1,6 +1,20 @@
 import * as express from 'express';
+import requestApi from '../../../../../../core/request-api';
 
 module.exports = (req: express.Request, res: express.Response): void => {
 	'use strict';
-	res.locals.display({}, 'i/album');
+
+	if (res.locals.file.folder !== null) {
+		requestApi('album/folders/show', {
+			'folder-id': res.locals.file.folder
+		}, req.user).then((folder: Object) => {
+			res.locals.display({
+				folder: folder
+			}, 'i/album');
+		});
+	} else {
+		res.locals.display({
+			folder: null
+		}, 'i/album');
+	}
 };
