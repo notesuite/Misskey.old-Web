f1e5921b1ce897a1d05f7a52c480ad083702fd21
syuilo <syuilotan@yahoo.co.jp>
2016-05-04 16:23:41 +0900

    Clean up

--- src/core/request-api.ts
+++ src/core/request-api.ts
@@ -9,7 +9,6 @@ export default function requestApi(
 	user: any = null,
 	withFile: boolean = false
 ): Promise<any> {
-	
 	const userId: string = user !== null ? typeof user === 'string' ? user : user.id : null;
 
 	return new Promise<any>((resolve, reject) => {
