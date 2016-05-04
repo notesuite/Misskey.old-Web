b2aa9de9160adbc4555b3c5c12b6998a194d20ee
syuilo <syuilotan@yahoo.co.jp>
2016-05-04 16:28:23 +0900

    Fix bug

--- src/core/get-post-summary.ts
+++ src/core/get-post-summary.ts
@@ -1,30 +1,30 @@
 module.exports = function analyze(locale: any, post: any): string {
-		switch (post.type) {
-		case 'status':
-		case 'reply':
-			let text = !isEmpty(post.text) ? post.text : '';
-			if (!isEmpty(post.files) && post.files.length > 0) {
-				if (text !== '') {
-					text += ' ';
-				}
-				switch (locale) {
-					case 'ja':
-						text += `(${post.files.length}個のファイル)`;
-						break;
-					default:
-						if (post.files.length > 1) {
-							text += `(${post.files.length} files)`;
-						} else {
-							text += `(${post.files.length} file)`;
-						}
-						break;
-				}
+	switch (post.type) {
+	case 'status':
+	case 'reply':
+		let text = !isEmpty(post.text) ? post.text : '';
+		if (!isEmpty(post.files) && post.files.length > 0) {
+			if (text !== '') {
+				text += ' ';
 			}
-			return text;
-		case 'repost':
-			return `RP ${analyze(locale, post.post)}`;
-		default:
-			break;
+			switch (locale) {
+				case 'ja':
+					text += `(${post.files.length}個のファイル)`;
+					break;
+				default:
+					if (post.files.length > 1) {
+						text += `(${post.files.length} files)`;
+					} else {
+						text += `(${post.files.length} file)`;
+					}
+					break;
+			}
+		}
+		return text;
+	case 'repost':
+		return `RP ${analyze(locale, post.post)}`;
+	default:
+		throw 'unknown post type';
 	}
 };
 
