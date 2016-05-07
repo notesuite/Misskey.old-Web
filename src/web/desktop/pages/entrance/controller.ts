3c069f345084885574b1121be174c8f3d4a8759e
syuilo <syuilotan@yahoo.co.jp>
2016-05-07 23:14:24 +0900

    :pizza:

--- src/web/desktop/pages/entrance/controller.ts
+++ src/web/desktop/pages/entrance/controller.ts
@@ -1,10 +1,5 @@
 import * as express from 'express';
-import requestApi from '../../../../core/request-api';
 
 module.exports = (req: express.Request, res: express.Response): void => {
-	requestApi('hashtags/trend/show', {}).then((hashtags: string[]) => {
-		res.locals.display({
-			trends: hashtags.map(hashtag => `#${hashtag}`)
-		});
-	});
+	res.locals.display({});
 };
