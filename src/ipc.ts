20153d3b12d0ba7c3481446df8eb13e00d467073
syuilo <syuilotan@yahoo.co.jp>
2016-06-17 19:34:37 +0900

    ：ぴざ：

--- src/ipc.ts
+++ src/ipc.ts
@@ -2,16 +2,21 @@ import * as os from 'os';
 import * as cluster from 'cluster';
 const ipc = require('node-ipc');
 
+const getInfo = () => {
+	return {
+		machine: os.hostname(),
+		pid: process.pid,
+		uptime: process.uptime()
+	};
+};
+
 ipc.config.id = 'misskey-web';
 ipc.config.retry = 1000;
 ipc.config.silent = true;
 
 ipc.serve(() => {
 	ipc.server.on('connect', (socket: any) => {
-		ipc.server.emit(socket, 'misskey.info', {
-			machine: os.hostname(),
-			pid: process.pid
-		});
+		ipc.server.emit(socket, 'misskey.info', getInfo());
 	});
 });
 
@@ -23,3 +28,7 @@ cluster.on('fork', worker => {
 		ipc.server.broadcast('misskey.log', msg);
 	});
 });
+
+setInterval(() => {
+	ipc.server.broadcast('misskey.info', getInfo());
+}, 1000);
